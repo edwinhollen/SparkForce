9b77b658f4ceaf0d985e884c3a52f127005107f4
Edwin Hollen <edwinhollen@gmail.com>
2015-07-29 17:34:22 -0400

    Add some polygon rendering

--- build/sparkforce.js
+++ build/sparkforce.js
@@ -1,21 +1,139 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 var SparkForce = (function () {
     function SparkForce(canvas) {
+        this.title = 'SparkForce';
+        this.version = '0.0';
+        this.renderWidth = 160;
+        this.renderHeight = 120;
+        this.entities = [];
         console.log("Initializing");
-        document.title = SparkForce.TITLE + ' (' + SparkForce.VERSION + ')';
+        document.title = this.title + ' (' + this.version + ')';
         this.ctx = canvas.getContext('2d');
-        this.ctx.canvas.width = SparkForce.RENDER_WIDTH;
-        this.ctx.canvas.height = SparkForce.RENDER_HEIGHT;
+        this.ctx.canvas.width = this.renderWidth;
+        this.ctx.canvas.height = this.renderHeight;
+        for (var i = 0; i < 10; i++) {
+            this.entities.push(new Entity(new PositionComponent(chance.integer({ min: 0, max: this.renderHeight }), chance.integer({ min: 0, max: this.renderHeight })), new HitboxComponent(0, 0, chance.integer({ min: 2, max: 7 }), chance.integer({ min: 2, max: 7 }))));
+        }
         window.requestAnimationFrame(this.render.bind(this));
     }
     SparkForce.prototype.render = function () {
+        var _this = this;
         window.requestAnimationFrame(this.render.bind(this));
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-        var now = new Date().getTime();
+        this.ctx.fillStyle = 'orange';
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.entitiesWith([PositionComponent, HitboxComponent]).forEach(function (entity) {
+            var position = entity.getComponent(PositionComponent);
+            var hitbox = entity.getComponent(HitboxComponent);
+            var baseX = position.x + hitbox.x;
+            var baseY = position.y + hitbox.y;
+            _this.ctx.fillStyle = 'green';
+            _this.ctx.moveTo(baseX, baseY);
+            _this.ctx.beginPath();
+            hitbox.points.forEach(function (point) {
+                _this.ctx.lineTo(baseX + point.x, baseY + point.y);
+            });
+            _this.ctx.fill();
+            _this.ctx.closePath();
+        });
     };
-    SparkForce.TITLE = 'SparkForce';
-    SparkForce.VERSION = '0.0';
-    SparkForce.RENDER_WIDTH = 160;
-    SparkForce.RENDER_HEIGHT = 120;
+    SparkForce.prototype.entitiesWith = function (componentTypes) {
+        return this.entities.filter(function (entity) {
+            return entity.hasComponents(componentTypes);
+        });
+    };
     return SparkForce;
 })();
+var Entity = (function () {
+    function Entity() {
+        var _this = this;
+        var components = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            components[_i - 0] = arguments[_i];
+        }
+        this.components = [];
+        components.forEach(function (c) {
+            _this.components.push(c);
+        });
+    }
+    Entity.prototype.getComponents = function (t) {
+        return this.components.filter(function (c) {
+            return (c instanceof t);
+        });
+    };
+    Entity.prototype.getComponent = function (t) {
+        return this.getComponents(t)[0];
+    };
+    Entity.prototype.hasComponent = function (t) {
+        return this.getComponents(t).length > 0;
+    };
+    Entity.prototype.hasComponents = function (ts) {
+        var _this = this;
+        return ts.every(function (t) {
+            return _this.hasComponent(t);
+        });
+    };
+    return Entity;
+})();
+var Point = (function () {
+    function Point(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+    }
+    return Point;
+})();
+var Polygon = (function () {
+    function Polygon() {
+        var _this = this;
+        var points = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            points[_i - 0] = arguments[_i];
+        }
+        this.points = [];
+        points.forEach(function (p) {
+            _this.points.push(p);
+        });
+    }
+    return Polygon;
+})();
+var Rectangle = (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle(x, y, width, height) {
+        _super.call(this, new Point(x, y), new Point(x + width, y), new Point(x + width, y + height), new Point(x, y + height));
+    }
+    Object.defineProperty(Rectangle.prototype, "x", {
+        get: function () {
+            return this.points[0].x;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Rectangle.prototype, "y", {
+        get: function () {
+            return this.points[0].y;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Rectangle;
+})(Polygon);
+var PositionComponent = (function (_super) {
+    __extends(PositionComponent, _super);
+    function PositionComponent(x, y) {
+        _super.call(this, x, y);
+    }
+    return PositionComponent;
+})(Point);
+var HitboxComponent = (function (_super) {
+    __extends(HitboxComponent, _super);
+    function HitboxComponent(x, y, width, height) {
+        _super.call(this, x, y, width, height);
+    }
+    return HitboxComponent;
+})(Rectangle);
 //# sourceMappingURL=sparkforce.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
 declare var chance:any;
 
 class SparkForce{
-	private static TITLE : string = 'SparkForce';
-	private static VERSION : string = '0.0';
-	private static RENDER_WIDTH : number = 160;
-	private static RENDER_HEIGHT : number = 120;
+	private title : string = 'SparkForce';
+	private version : string = '0.0';
+	private renderWidth : number = 160;
+	private renderHeight : number = 120;
+	
+	private entities: Entity[] = [];
 	
 	private ctx : CanvasRenderingContext2D;
 	constructor(canvas: HTMLCanvasElement){
 		console.log("Initializing");
-		document.title = SparkForce.TITLE + ' (' + SparkForce.VERSION + ')';
+		document.title = this.title + ' (' + this.version + ')';
 		
 		this.ctx = canvas.getContext('2d');
-		this.ctx.canvas.width = SparkForce.RENDER_WIDTH;
-		this.ctx.canvas.height = SparkForce.RENDER_HEIGHT;
+		this.ctx.canvas.width = this.renderWidth;
+		this.ctx.canvas.height = this.renderHeight;
+		
+		for(var i = 0; i < 10; i++){
+			this.entities.push(new Entity(
+				new PositionComponent(chance.integer({min:0, max:this.renderHeight}), chance.integer({min:0, max:this.renderHeight})),
+				new HitboxComponent(0, 0, chance.integer({min:2, max:7}), chance.integer({min:2, max:7}))
+			));
+		}
 	
 		window.requestAnimationFrame(this.render.bind(this));
 	}
@@ -21,7 +30,112 @@ class SparkForce{
 	render(){
 		window.requestAnimationFrame(this.render.bind(this));
 		this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-		var now : number = new Date().getTime();
+		
+		this.ctx.fillStyle = 'orange';
+		this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+		
+		this.entitiesWith([PositionComponent, HitboxComponent]).forEach(entity => {
+			let position: PositionComponent = entity.getComponent<PositionComponent>(PositionComponent);
+			let hitbox: HitboxComponent = entity.getComponent<HitboxComponent>(HitboxComponent);
+			
+			let baseX = position.x + hitbox.x;
+			let baseY = position.y + hitbox.y;
+			
+			this.ctx.fillStyle = 'green';
+			this.ctx.moveTo(baseX, baseY);
+			this.ctx.beginPath();
+			hitbox.points.forEach(point => {
+				this.ctx.lineTo(baseX + point.x, baseY + point.y);
+			});
+			this.ctx.fill();
+			this.ctx.closePath();
+		});
+	}
+	
+	entitiesWith(componentTypes: any[]): Entity[]{
+		return this.entities.filter(entity => {
+			return entity.hasComponents(componentTypes);
+		});
+	}
+}
+
+class Entity{
+	private components: Component[] = [];
+	constructor(...components: Component[]){
+		components.forEach(c => {
+			this.components.push(c);
+		});
+	}
+	getComponents <T> (t: any): T[]{
+		return <T[]> this.components.filter(c => {
+			return (c instanceof t);
+		});
+	}
+	getComponent <T> (t: any): T{
+		return this.getComponents<T>(t)[0];
+	}
+	hasComponent (t: any): boolean{
+		return this.getComponents(t).length > 0;
+	}
+	hasComponents (ts: any[]): boolean{
+		return ts.every(t => {
+			return this.hasComponent(t);
+		});
+	}
+}
+
+interface Component{
+	
+}
+
+class Point{
+	public x: number; 
+	public y: number;
+	constructor(x, y){
+		this.x = x || 0;
+		this.y = y || 0;
+	}
+}
+
+
+
+class Polygon{
+	public points: Point[] = [];
+	constructor(...points:Point[]){
+		points.forEach(p => {
+			this.points.push(p);
+		});
+	}
+}
+
+class Rectangle extends Polygon{
+	constructor(x: number, y: number, width: number, height: number){
+		super(
+			new Point(x, y),
+			new Point(x+width, y),
+			new Point(x+width, y+height),
+			new Point(x, y+height)
+		);
+	}
+	
+	get x(){
+		return this.points[0].x;
+	}
+	
+	get y(){
+		return this.points[0].y;
+	}
+}
+
+class PositionComponent extends Point implements Component{
+	constructor(x, y){
+		super(x, y);
+	}
+}
+
+class HitboxComponent extends Rectangle implements Component{
+	constructor(x: number, y: number, width: number, height: number){
+		super(x, y, width, height);
 		
 	}
 }
